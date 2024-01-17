3801e16a0b9bb50201ccb64b195fbf5f376d2590
Nando <84581167+Sampasbc@users.noreply.github.com>
2024-01-17 20:44:47 +0000

    Fix logo functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      <a href="#business">
+      <a href="#root">
         <img className="logo" src="/logo_big_white.png" alt="SavorQuest Logo" />
       </a>
     </div>
