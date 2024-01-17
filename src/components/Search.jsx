fde691b339dc7726f8ac9bdd4789a31853293239
Nando <84581167+Sampasbc@users.noreply.github.com>
2024-01-17 22:20:39 +0000

    Fix responsive

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,12 +10,14 @@ function Search() {
             type="text"
             id="category"
             placeholder="Category"
+            required
           />
           <input
             className="search_input"
             type="text"
             id="location"
             placeholder="Location"
+            required
           />
         </div>
         <button type="submit">Search</button>
